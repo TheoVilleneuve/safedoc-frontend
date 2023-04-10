95e6b165690f537fb5027e597ee6f92c24c471af
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-04-10 16:32:28 +0200

    t login page almost ok

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,41 +2,53 @@ import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
 
 
 export default function LoginScreen({ navigation }) {
-    return (
-        <View style={styles.container}>
+  return (
+    <View style={styles.container}>
 
-          <Image style={styles.logoSafeDoc} source={require('../assets/logoSafeDoc.png')} />
+      <View style={styles.logoContainer}>
+        <Image style={styles.logoSafeDoc} source={require('../assets/logoSafeDoc.png')} />
+        <Text style={styles.h2}>Niquez-vous les cis-hét !</Text>
+      </View>
 
-          <View style={styles.btnContainer}>
+        <View style={styles.btnContainer}>
           {/* bouton connection */}
-            <TouchableOpacity
-            style={styles.largeButton}
-            title="Go to SignIn"
-            onPress={() => navigation.navigate('SignIn')}
-            >
-                <Text style={styles.h3}>Me connecter</Text>
-            </TouchableOpacity>
+          <TouchableOpacity
+          style={styles.largeBtn}
+          title="Go to SignIn"
+          onPress={() => navigation.navigate('SignIn')}
+          >
+              <Text style={styles.h3}>Me connecter</Text>
+          </TouchableOpacity>
 
-            {/* bouton insription */}
-            <TouchableOpacity
-            style={styles.largeBtn}
-            title="Go to SignUp"
-            onPress={() => navigation.navigate('SignUp')}
-            >
-                <Text style={styles.h3}>M'inscrire</Text>
-            </TouchableOpacity>
+          {/* bouton insription */}
+          <TouchableOpacity
+          style={styles.largeBtn}
+          title="Go to SignUp"
+          onPress={() => navigation.navigate('SignUp')}
+          >
+              <Text style={styles.h3}>M'inscrire</Text>
+          </TouchableOpacity>
 
-            {/* Accès sans compte */}
-            <TouchableOpacity
-            style={styles.mediumBtn}
-            title="Go with no account"
-            onPress={() => navigation.navigate('NoAccount')}
-            >
-                <Text style={styles.h3}>Accès sans compte</Text>
-            </TouchableOpacity>
-          </View>
         </View>
-      );
+
+          {/* Accès sans compte */}
+          <TouchableOpacity
+          style={styles.mediumBtn}
+          title="Go with no account"
+          onPress={() => navigation.navigate('NoAccount')}
+          >
+              <Text style={styles.h3}>Accès sans compte</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+          style={styles.contact}
+          title="Go to infos"
+          onPress={() => navigation.navigate('InfosScreen')}
+          >
+          <Text>Qui sommes-nous ?</Text>
+          </TouchableOpacity>        
+    </View>
+  );
 
 }
 
@@ -45,38 +57,101 @@ const styles = StyleSheet.create({
       flex: 1,
       display: 'flex',
       flexDirection: 'column',
-      justifyContent: 'flex-start',
+      justifyContent: 'space-between',
       alignItems: 'center',
+      },
+    
+    logoContainer: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      top: 100,
     },
-    btnContainer: {
-      backgroundColor: 'red',
-      width: '70%',
-    },
+    
     logoSafeDoc: {
       objectFit: 'contain',
-      width: '70%',
-      top: 20,
+      width: '85%',
+      height: 120,
+      },
+
+    h2: {
+      marginTop: 30,
+      position: 'absolute',
+      color: '#2D0861',
+      fontFamily: 'Greycliff CF',
+      fontStyle: 'normal',
+      fontWeight: 800,
+      fontSize: 16,
+      lineHeight: 19,
+      display: 'flex',
+      alignItems: 'center',
+      letterSpacing: 0.25,
+      top: 100,
+    },
+
+    btnContainer: {
+      position: 'absolute',
+      display: 'flex',
+      height: '25%',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      bottom: 250,
     },
     largeBtn: {
-      color: 'white',
-      backgroundColor: '#2D0861',
-      width: '100%',
-      height: '10%',
-      justifyContent: 'center',
+      display: 'flex',
       alignItems: 'center',
+      justifyContent: 'center',
+      /* Purple */
+      backgroundColor: '#2D0861',
+      width: 320,
+      height: 68,
+      borderRadius: 20,
+      /* Shadow Boutons */
+      shadowColor: "#000000",
+      shadowOffset: {
+      width: 6,
+      height: 6,
+      },
+      shadowOpacity:  0.25,
+      shadowRadius: 12,
+      elevation: 12
     },
     mediumBtn: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        /* Purple /
-        backgroundColor: '#652CB3',
-        width: 182,
-        height: 68,
-        borderRadius: 20,
-        / Shadow Boutons */
+      position: 'absolute',
+      bottom: 100,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      /* Purple */
+      backgroundColor: '#652CB3',
+      width: 182,
+      height: 68,
+      borderRadius: 20,
+      /* Shadow Boutons */
+      shadowColor: "#000000",
+      shadowOffset: {
+      width: 6,
+      height: 6,
       },
+      shadowOpacity:  0.25,
+      shadowRadius: 12,
+      elevation: 12
+      },
+    contact: {
+      color: '#2D0861',
+      bottom: 60,
+    },
+    
     h3: {
       color: 'white',
-    }
+      fontFamily: 'Greycliff CF',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      fontSize: 16,
+      lineHeight: 19,
+      display: 'flex',
+      alignItems: 'center',
+      letterSpacing: 0.25,
+    },
   });
