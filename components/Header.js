b3e0d521b16ea7cc0914033e82cf8bf84f774ee3
Julien Dominguez <dominguez.julien@gmail.com>
2023-04-11 18:55:09 +0200

    j dossier component

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+// import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+
+
+// export default function Header() {
+//     return (
+//         <View style={styles.container}>
+//             <TouchableOpacity
+//             title="Go to Quiz Orientation"
+//             onPress={() => navigation.navigate('QuizOrientation')}
+//             >
+//                 <Text>Home Screen</Text>
+//             </TouchableOpacity>
+//         </View>
+//       );
+
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: 'yellow',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//   });
